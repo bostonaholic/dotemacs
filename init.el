037bf6b61ea1653dbcf3e2e7890bf5e652306dc2
Matthew Boston <matthew@matthewboston.com>
2017-10-24 18:36:17 -0600

    initial commit

---
+++ init.el
@@ -0,0 +1,36 @@
+;;; init.el
+
+;; Copyright (C) 2017 Matthew Boston
+;; Created: October 24, 2017
+;; Homepage: https://github.com/bostonaholic/dotemacs
+
+;;; Code:
+
+(let ((gc-cons-threshold most-positive-fixnum))
+
+  (require 'package)
+  (setq-default load-prefer-newert t
+                package-enable-at-startup nil)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (package-initialize)
+
+  ;; Install dependencies
+  (unless (and (package-installed-p 'delight)
+               (package-installed-p 'use-package))
+    (package-refresh-contents)
+    (package-install 'delight t)
+    (package-install 'use-package t))
+  (setq-default use-package-always-defer t
+                use-package-always-ensure t)
+
+  ;; Use latest Org
+  (use-package org
+    :pin org
+    :ensure org-plus-contrib)
+
+  ;; Tangle configuration
+  (org-babel-load-file (expand-file-name "dotemacs.org" user-emacs-directory))
+  (garbage-collect))
+
+;;; init.el ends here
