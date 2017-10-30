acb36a3c2a905b91785f2ae4531e7329f8b97930
Matthew Boston <matthew@matthewboston.com>
2017-10-30 13:00:54 -0600

    whitespace

--- lisp/helm.el
+++ lisp/helm.el
@@ -4,23 +4,27 @@
   :defer 1
   :preface (load-file (expand-file-name "lisp/helm-funcs.el" user-emacs-directory))
   :commands (msb/helm-find-files)
+
   :init
   (add-hook 'helm-cleanup-hook #'msb//helm-cleanup)
   (define-key minibuffer-local-map (kbd "C-c C-l") 'helm-minibuffer-history)
+
   :config
   (helm-mode)
   (helm-locate-set-command)
   (setq helm-locate-fuzzy-match (string-match "locate" helm-locate-command))
+
   :bind
   (("C-x C-f" . msb/helm-find-files)
    ("C-x b" . helm-buffers-list)))
 
 (use-package helm-ag
   :after helm
+  :preface (defvar search-tools '("ag" "pt" "ack" "grep"))
+
   :bind (("C-c /" . msb/helm-project-smart-do-search)
          ("C-c *" . msb/helm-project-smart-do-search-region-or-symbol))
-  :preface
-  (defvar search-tools '("ag" "pt" "ack" "grep"))
+
   :init
   ;; This overrides the default C-s action in helm-projectile-switch-project
   ;; to search using ag/pt/whatever instead of just grep
