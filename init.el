be325a2222b89b9604c64cb7dca95c3907d7d9e8
Matthew Boston <matthew@matthewboston.com>
2018-01-05 18:23:13 -0700

    set package archives to https

--- init.el
+++ init.el
@@ -11,8 +11,8 @@
   (require 'package)
   (setq-default load-prefer-newer t
                 package-enable-at-startup nil)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
   (package-initialize)
 
   ;; Install dependencies
